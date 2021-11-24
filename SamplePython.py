551ceb24f742fcdbc09c71d5854e8f79d873e038
itsmanojreddyy <94695026+itsmanojreddyy@users.noreply.github.com>
2021-11-24 21:53:54 +0530

    Update SamplePython.py

--- SamplePython.py
+++ SamplePython.py
@@ -6,6 +6,6 @@ class TestStringMethods(unittest.TestCase):
 
     def test_Sample(self):
       print("Execution for Azure Devops")
-      self.assertEqual("1","1","Values not as per expectation @@")
+      self.assertEqual("1","2","Values not as per expectation @@")
 if __name__ == "__main__":
     unittest.main()
