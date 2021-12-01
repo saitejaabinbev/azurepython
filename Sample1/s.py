266c2d0b5b1006ff9860e4aedbfa86533af71c68
itsmanojreddyy <94695026+itsmanojreddyy@users.noreply.github.com>
2021-12-01 10:24:56 +0530

    Update s.py

--- Sample1/s.py
+++ Sample1/s.py
@@ -1,11 +1,19 @@
 
 import unittest
 import os
+import json
 
 class TestStringMethods(unittest.TestCase):
 
     def test_Sample(self):
+        
       print("Execution for Azure Devops")
+      f = open('config.json',)
+      data = json.load(f)
+      username = data['username']
+      password = data['password']
+      print("username : ",username)
+      print("Data : ",data)
       print(os.environ.get("h"))
       self.assertEqual("1","1","Values not as per expectation @@")
 if __name__ == "__main__":
